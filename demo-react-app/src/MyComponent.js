f3aac5e0bf344946802594b135d74c625178774f
alanniedecena <alannie.khem.decena@gmail.com>
2023-03-17 15:50:34 +1300

    mob6

---
+++ demo-react-app/src/MyComponent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+class MyComponent extends React.Component {
+    render() {
+        return <h1>hello World</h1>
+    }
+}
+
+export default MyComponent
---
+++ demo-react-app/src/ProsDisplayer.js
@@ -0,0 +1,18 @@
+import React from "react";
+class PropsDisplayer extends React.Component {
+    render() {
+        const stringProps = JSON.stringify(this.props);
+
+        return (
+            <div>
+                <h1>Props Displayer</h1>
+                <h2>{stringProps}</h2>
+                <p>Name: {this.props.name}</p>
+                <p>Location: {this.props.location}</p>
+                <p>Number: {this.props.number}</p>
+            </div>
+        );
+    }
+}
+
+export default PropsDisplayer
